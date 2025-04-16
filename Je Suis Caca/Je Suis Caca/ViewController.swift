87827585f6c034ac0ce0147c07f2f55f803a0a33
42JINAM <jinam@student.42.fr>
2025-04-16 23:59:47 +0200

    changing background

--- Je Suis Caca/Je Suis Caca/ViewController.swift
+++ Je Suis Caca/Je Suis Caca/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var screen: UIView!
+    let colors: [UIColor] = [
+        UIColor(red:58/255.0, green: 89/255.0, blue: 209/255.0, alpha: 1),
+        UIColor(red:61/255.0, green: 144/255.0, blue: 215/255.0, alpha: 1),
+        UIColor(red:122/255.0, green: 198/255.0, blue: 210/255.0, alpha: 1),
+        UIColor(red:181/255.0, green: 252/255.0, blue: 205/255.0, alpha: 1),
+        UIColor(red:214/255.0, green: 123/255.0, blue: 255/255.0, alpha: 1),
+        ]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeScreenColor(_ sender: Any) {
+        screen.backgroundColor = colors.randomElement()
+    }
+    
 }
 
