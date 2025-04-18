5c70c26f871a50cbc5bad416ae1e310bc5f5ac7f
42JINAM <jinam@student.42.fr>
2025-04-18 18:13:08 +0200

    haptic & sound

---
+++ Xylophone/Xylophone/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Angela Yu on 28/06/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
+    let generator = UIImpactFeedbackGenerator(style: .medium)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func keyPressed(_ sender: UIButton) {
+        sender.alpha = 0.5
+        generator.impactOccurred()
+        playSound(sender.currentTitle!)
+        let timer = Timer.scheduledTimer(withTimeInterval: 0.2, repeats: false) {
+            (timer) in sender.alpha = 1
+        }
+        
+    }
+    
+    func playSound(_ resource: String) {
+        let url = Bundle.main.url(forResource: resource, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
+}
+
+
