5bfc4f124b5aecd1b5765483dd08b517e17abf3d
42JINAM <jinam@student.42.fr>
2025-04-28 22:21:35 +0200

    add UPickerViewDataSource

--- ByteCoin/ByteCoin/Controller/ViewController.swift
+++ ByteCoin/ByteCoin/Controller/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bitcoinLabel: UILabel!
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    let coinManager = CoinManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        currencyPicker.dataSource = self
         // Do any additional setup after loading the view.
     }
 
 
 }
 
+extension ViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+       return (1)
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return (coinManager.currencyArray.count)
+    }
+}
