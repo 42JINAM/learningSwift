00828b9889904480b918c4061229d791866c8fc6
42JINAM <jinam@student.42.fr>
2025-04-29 15:20:50 +0200

    lable animation

--- Flash-Chat/Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash-Chat/Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -9,14 +9,27 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
-
+    
     @IBOutlet weak var titleLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        
+        titleLabel.text = ""
+        let titleText = "⚡️FlashChat"
+        titleLabel.showTitleAnimation(title: titleText)
     }
-    
+}
 
+extension UILabel {
+    func showTitleAnimation(title : String) {
+        var charIndex : Double = 0
+        for letter in title {
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { timer in
+                self.text?.append(letter)
+            }
+            charIndex += 1
+        }
+        
+    }
 }
