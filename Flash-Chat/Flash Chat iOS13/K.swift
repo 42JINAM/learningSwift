104aeb36f64b6db70b83173dd06863164e9d1245
42JINAM <jinam@student.42.fr>
2025-05-02 14:35:05 +0200

    Constants

---
+++ Flash-Chat/Flash Chat iOS13/K.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Flash Chat iOS13
+//
+//  Created by Private on 02/05/2025.
+//  Copyright © 2025 Angela Yu. All rights reserved.
+//
+
+struct K {
+    static let registerSegue = "RegisterToChat"
+    static let loginSegue = "LoginToChat"
+
+}
