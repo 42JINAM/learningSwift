9965e1950dc856326e13f89d32f77ab8fa7d24de
42JINAM <jinam@student.42.fr>
2025-05-05 22:00:49 +0200

    feat: hacker news text list

---
+++ HackerNews/HackerNews/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  HackerNews
+//
+//  Created by Private on 05/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            List(posts, rowContent: { post in
+                Text(post.title)
+            })
+            .navigationBarTitle("Hacker News")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
+struct Post: Identifiable {
+    let id: String
+    let title: String
+}
+
+let posts = [
+    Post(id: "1", title: "Hello"),
+    Post(id: "2", title: "Bonjour"),
+    Post(id: "3", title: "Hola"),
+]
---
+++ HackerNews/HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Private on 05/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
