8e40a12f101ffb152ad71b9d3bd9c1e9a92434ad
42JINAM <jinam@student.42.fr>
2025-05-06 21:02:00 +0200

    AsyncImage
    load Image, scale image, using extension, animation

---
+++ SwiftUI/SwiftUIAsyncImage/SwiftUIAsyncImage/SwiftUIAsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAsyncImageApp.swift
+//  SwiftUIAsyncImage
+//
+//  Created by Private on 06/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
