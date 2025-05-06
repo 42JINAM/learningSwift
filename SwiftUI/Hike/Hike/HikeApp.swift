d21f99667b39806ac62f87159a56f1d5b69e0a68
42JINAM <jinam@student.42.fr>
2025-05-06 21:42:11 +0200

    feat: Initial project setup

---
+++ SwiftUI/Hike/Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Private on 06/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
