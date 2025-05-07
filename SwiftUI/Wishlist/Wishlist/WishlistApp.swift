751138f8d0416b4e9babad41ee4416a7e4e9de67
42JINAM <jinam@student.42.fr>
2025-05-07 21:02:34 +0200

    chore: add app icon

---
+++ SwiftUI/Wishlist/Wishlist/WishlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  WishlistApp.swift
+//  Wishlist
+//
+//  Created by Private on 07/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct WishlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
