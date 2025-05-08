b45fef897e4013e320f9f5706771c2b5226fefdd
42JINAM <jinam@student.42.fr>
2025-05-08 16:01:59 +0200

    feat: implement add item

--- SwiftUI/Grocery List/Grocery List/ContentView.swift
+++ SwiftUI/Grocery List/Grocery List/ContentView.swift
@@ -11,6 +11,8 @@ import SwiftData
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var items: [Item]
+    @State private var item: String = ""
+    @FocusState private var isFocused: Bool
     
     func addEssentialFood() {
         modelContext.insert(Item(title: "Bakery & Bread", isCompleted: false))
@@ -70,6 +72,34 @@ struct ContentView: View {
                     )
                 }
             }
+            .safeAreaInset(edge: .bottom) {
+                VStack(spacing: 12) {
+                    TextField("", text: $item)
+                        .textFieldStyle(.plain)
+                        .padding(12)
+                        .background(.tertiary)
+                        .cornerRadius(12)
+                        .font(.title.weight(.light))
+                        .focused($isFocused)
+                    Button {
+                        guard !item.isEmpty else { return }
+                        let newItem = Item(title: item, isCompleted: false)
+                        modelContext.insert(newItem)
+                        item = ""
+                        isFocused = false
+                    } label: {
+                        Text("Save")
+                            .font(.title2.weight(.medium))
+                            .frame(maxWidth: .infinity)
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .buttonBorderShape(.roundedRectangle)
+                    .controlSize(.extraLarge)
+                }
+                .padding()
+                .background(.bar)
+            }
+            
         }
     }
 }
@@ -84,7 +114,7 @@ struct ContentView: View {
         Item(title: "Bakery & Bread", isCompleted: false),
         Item(title: "Meat & Seafood", isCompleted: true),
         Item(title: "Creal", isCompleted: .random()),
-        ]
+    ]
     let container = try! ModelContainer(
         for: Item.self, configurations: ModelConfiguration(isStoredInMemoryOnly: true))
     for item in sampleData {
