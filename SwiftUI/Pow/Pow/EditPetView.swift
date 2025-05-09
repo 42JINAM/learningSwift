f0388b5db352bbf821c8c3bdc24b07eed0d8041e
42JINAM <jinam@student.42.fr>
2025-05-09 11:52:52 +0200

    feat: Implement Edit data view

---
+++ SwiftUI/Pow/Pow/EditPetView.swift
@@ -0,0 +1,18 @@
+//
+//  EditPetView.swift
+//  Pow
+//
+//  Created by Private on 09/05/2025.
+//
+
+import SwiftUI
+
+struct EditPetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditPetView()
+}
