996d62416902044cf31df1e7a03d2dd7df192b3a
42JINAM <jinam@student.42.fr>
2025-05-09 10:06:59 +0200

    feat: Implement pet grid

--- SwiftUI/Pow/Pow/ContentView.swift
+++ SwiftUI/Pow/Pow/ContentView.swift
@@ -12,11 +12,53 @@ struct ContentView: View {
     @Environment(\.modelContext) var modelContext
     @Query private var pets: [Pet]
     
+    let layout = [
+        GridItem(.flexible(minimum: 120)),
+        GridItem(.flexible(minimum: 120)),
+    ]
+    
     var body: some View {
         NavigationStack {
             ScrollView {
-                
+                LazyVGrid(columns: layout) {
+                    GridRow {
+                        ForEach(pets) { pet in
+                            NavigationLink(destination: EmptyView()) {
+                                VStack {
+                                    if let imageData = pet.photo {
+                                        if let image = UIImage(data: imageData) {
+                                            Image(uiImage: image)
+                                        }
+                                    } else {
+                                        Image(systemName: "pawprint.circle")
+                                            .resizable()
+                                            .scaledToFit()
+                                            .padding(40)
+                                            .foregroundStyle(.quaternary)
+                                    }
+                                    Spacer()
+                                    Text(pet.name)
+                                        .font(.title.weight(.light))
+                                        .padding(.vertical)
+                                    Spacer()
+                                }
+                                .frame(
+                                    minWidth: 0,
+                                    maxWidth: .infinity,
+                                    minHeight: 0,
+                                    maxHeight: .infinity,
+                                )
+                                .background(.ultraThinMaterial)
+                                .clipShape(RoundedRectangle(cornerRadius: 8, style: .circular))
+                            }
+                            .foregroundStyle(.primary)
+                        }
+                    } //Grid row
+                    
+                } // Grid layout
+                .padding(.horizontal)
             } // scrollview
+            .navigationTitle(pets.isEmpty ? "" : "Paws")
             .overlay {
                 if pets.isEmpty {
                     CustomContentUnavailableView(
@@ -35,6 +77,7 @@ struct ContentView: View {
         .modelContainer(for: Pet.self, inMemory: true)
 }
 
-#Preview {
+#Preview("Sample data") {
     ContentView()
+        .modelContainer(Pet.preview)
 }
