2d5c7eb9e5af580ae641c1cef20331ff6a7383a6
42JINAM <jinam@student.42.fr>
2025-05-09 12:02:49 +0200

    feat : add EditPetView

--- SwiftUI/Pow/Pow/EditPetView.swift
+++ SwiftUI/Pow/Pow/EditPetView.swift
@@ -6,13 +6,86 @@
 //
 
 import SwiftUI
+import SwiftData
+import PhotosUI
 
 struct EditPetView: View {
+    
+    @Bindable var pet: Pet
+    @State private var photosPickerItem: PhotosPickerItem?
+    
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            //MARK: - Image
+            if let imageData = pet.photo {
+                if let image = UIImage(data: imageData) {
+                    Image(uiImage: image)
+                        .resizable()
+                        .scaledToFit()
+                        .clipShape(RoundedRectangle(cornerRadius: 8, style: .circular))
+                        .frame(
+                            minWidth: 0,
+                            maxWidth: .infinity,
+                            minHeight: 0,
+                            maxHeight: 300
+                        )
+                        .padding(.top)
+                }
+            } else {
+                CustomContentUnavailableView(icon: "pawprint.circle", title: "No Photo", description: "Add a photo of your favorite pet to make it easier to find them.")
+                    .padding(.top)
+            }
+            //MARK: - photo picker
+            PhotosPicker(selection: $photosPickerItem, matching: .images) {
+                Label("Select a Photo", systemImage: "photo.badge.plus")
+                    .frame(minWidth: 0, maxWidth: .infinity)
+            }
+            .listRowSeparator(.hidden)
+            
+            //MARK: - text field
+            TextField("Name", text: $pet.name)
+                .textFieldStyle(.roundedBorder)
+                .font(.largeTitle.weight(.light))
+                .padding(.vertical)
+            //MARK: - button
+            Button {
+                
+            } label: {
+                Text("Save")
+                    .font(.title3.weight(.medium))
+                    .padding(8)
+                    .frame(minWidth: 0, maxWidth: .infinity)
+            }
+            .buttonStyle(.borderedProminent)
+            .listRowSeparator(.hidden)
+            .navigationBarTitleDisplayMode(.inline)
+            .onChange(of: photosPickerItem) {
+                Task {
+                    pet.photo = try? await photosPickerItem?.loadTransferable(type: Data.self)
+                }
+            }
+
+        } //form
+        .listStyle(.plain)
+        .navigationTitle("Edit \(pet.name)")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 #Preview {
-    EditPetView()
+    NavigationStack {
+        do {
+            let configuration = ModelConfiguration(isStoredInMemoryOnly: true)
+            let container = try ModelContainer(
+                for: Pet.self,
+                configurations: configuration
+            )
+            let sampleData = Pet(name: "Daisy")
+            return EditPetView(pet: sampleData)
+                .modelContainer(container)
+        } catch {
+            fatalError("\(error)")
+        }
+    }
 }
