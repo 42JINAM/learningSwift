8a9b32e47bde78d76c393349bb1eee4c2f073fcd
42JINAM <jinam@student.42.fr>
2025-06-02 14:42:00 +0200

    tabview

---
+++ TabViewDemo/TabViewDemo/TabViewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewDemoApp.swift
+//  TabViewDemo
+//
+//  Created by Private on 02/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct TabViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
